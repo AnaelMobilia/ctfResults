8b84517d260a3f5299f24d1d1ceec51abe2955f0
Anael MOBILIA <contrib@anael.eu>
2019-04-13 00:16:06 +0200

    Clean du code

--- admin.php
+++ admin.php
@@ -136,16 +136,5 @@ $listeFlags = loader::chargerTypeActions(true, false);
         </footer>
         <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
         <script src="js/bootstrap.bundle.min.js" integrity="sha384-xrRywqdh3PHs8keKZN+8zzc5TX0GRTLCcmivcbNJWm2rs5C8PRhcEn3czEjhAO9o" crossorigin="anonymous"></script>
-        <script src="js/jquery.dataTables.min.js"></script>
-        <script>
-            // Tri du tableau....
-            $(document).ready(function () {
-                $('#resultats').DataTable({
-                    paging: false,
-                    searching: false,
-                    info: false
-                });
-            });
-        </script>
     </body>
 </html>
